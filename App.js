45302ee62fcbe4e7a7c695e973b91c21f822581f
Jameeru <144582911+Jameeru@users.noreply.github.com>
2024-07-31 16:28:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,24 +2,9 @@ import React from 'react';
 import StudentListComponent from './StudentListCompo.js';
 
 const studentList = [
-  {
-    id: 120,
-    fistName: "Naresh",
-    lastName: 'Kumar',
-    course: 'MECHANICAL',
-  },
-  {
-    id: 121,
-    fistName: "Muthu",
-    lastName: 'Ganesh',
-    course: 'CIVIL',
-  },
-  {
-    id: 122,
-    fistName: "Ritesh",
-    lastName: 'Arya',
-    course: 'MECHANICAL',
-  }
+  { id: 120, firstName: "Naresh", lastName: "Kumar", course: "MECHANICAL" },
+  { id: 121, firstName: "Muthu", lastName: "Ganesh", course: "CIVIL" },
+  { id: 122, firstName: "Ritesh", lastName: "Arya", course: "MECHANICAL" }
 ];
 
 function App() {
